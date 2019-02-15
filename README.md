fa7ef785d20b6b1d0def6237ccb99d2a4932305b
Martin Sköld <mskoldmath.su.se>
2019-02-15 19:41:48 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
-# Martens_et_al_2018
+Code for reproducing source apportionment in Martens et.al. (2019)
+*Global Biogeochemical Cycles*, 33, 2-14
+================
+
+### Statistical model
+
+See article supporting information for model details implemented in
+[`model.jags`](model.jags).
+
+### Preparation
+
+``` r
+library(tidyverse)
+library(readxl)
+library(rjags)
+library(cowplot)
+# Data file downloaded from Bolin center database 2019-02-15 using
+# download.file("https://bolin.su.se/data/uploads/Martens-2018-3.xlsx", destfile = "Martens-2018-3.xlsx", mode = "wb")
+isotope_data <- read_excel("Martens-2018-3.xlsx", 
+                           sheet = "S3", range = "A3:H64",
+                           col_names = c("depth", "age", "OC", "d13C", "OC_flux", "lab", "D14C_raw", "D14C_corrected")) %>% 
+  select(age, d13C, D14C = D14C_corrected)
+# End member values from Article SI tables S6-S9
+end_members <- read_csv("data/end_members.csv")
+# Helper functions
+source("functions.R")
+```
+
+### Model fitting using MCMC (JAGS)
+
+``` r
+# Parameters for MCMC
+N <- 10000000
+thin <- 1000
+# Young part of the core
+isotopes_young <- filter(isotope_data, age < 8000)
+em_young <- filter(end_members, Source != "M_old")
+# Run MCMC on young part
+out_young <-  run_jags(isotopes_young, "model.jags", em_young, N, thin)
+
+# Old part of the core
+isotopes_old <- filter(isotope_data, age > 10000)
+em_old <- filter(end_members, Source != "M_young")
+# Run MCMC on young part
+out_old <-  run_jags(isotopes_old, "model.jags", em_old, N, thin)
+```
+
+### Visualisation of results
+
+``` r
+plot_p(out_young, out_old, isotopes_young, isotopes_old) +
+        annotate("text", x = c(5000, 4500, 5000), y = c(0.15, 0.33, 0.5), 
+                 color = c("olivedrab3", "darkorange1", "cadetblue3"),
+                 label = c("Active Layer", "ICD", "Marine OC"),
+                 fontface="bold")
+```
+
+![](README_files/figure-gfm/unnamed-chunk-3-1.png)<!-- -->
+
+Posterior median proportions with 50% credibility regions.
+
+``` r
+plot_p2(out_young, out_old, isotopes_young, isotopes_old) 
+```
+
+![](README_files/figure-gfm/unnamed-chunk-4-1.png)<!-- -->
+
+Posterior proportion densities at D14C sampling ages.
+
+``` r
+sessionInfo()
+```
+
+    ## R version 3.5.1 (2018-07-02)
+    ## Platform: x86_64-w64-mingw32/x64 (64-bit)
+    ## Running under: Windows 10 x64 (build 17763)
+    ## 
+    ## Matrix products: default
+    ## 
+    ## locale:
+    ## [1] LC_COLLATE=Swedish_Sweden.1252  LC_CTYPE=Swedish_Sweden.1252   
+    ## [3] LC_MONETARY=Swedish_Sweden.1252 LC_NUMERIC=C                   
+    ## [5] LC_TIME=Swedish_Sweden.1252    
+    ## 
+    ## attached base packages:
+    ## [1] stats     graphics  grDevices utils     datasets  methods   base     
+    ## 
+    ## other attached packages:
+    ##  [1] readxl_1.1.0    bindrcpp_0.2.2  cowplot_0.9.3   rjags_4-8      
+    ##  [5] coda_0.19-2     forcats_0.3.0   stringr_1.3.1   dplyr_0.7.8    
+    ##  [9] purrr_0.2.5     readr_1.2.1     tidyr_0.8.2     tibble_2.0.1   
+    ## [13] ggplot2_3.1.0   tidyverse_1.2.1
+    ## 
+    ## loaded via a namespace (and not attached):
+    ##  [1] tidyselect_0.2.5 haven_2.0.0      lattice_0.20-35  colorspace_1.3-2
+    ##  [5] generics_0.0.2   htmltools_0.3.6  yaml_2.2.0       rlang_0.3.1     
+    ##  [9] pillar_1.3.1     glue_1.3.0       withr_2.1.2      modelr_0.1.2    
+    ## [13] bindr_0.1.1      plyr_1.8.4       munsell_0.5.0    gtable_0.2.0    
+    ## [17] cellranger_1.1.0 rvest_0.3.2      codetools_0.2-15 evaluate_0.12   
+    ## [21] labeling_0.3     knitr_1.20       broom_0.5.1      Rcpp_1.0.0      
+    ## [25] scales_1.0.0     backports_1.1.2  jsonlite_1.6     hms_0.4.2       
+    ## [29] digest_0.6.18    stringi_1.2.4    grid_3.5.1       cli_1.0.1       
+    ## [33] tools_3.5.1      magrittr_1.5     lazyeval_0.2.1   crayon_1.3.4    
+    ## [37] pkgconfig_2.0.2  xml2_1.2.0       lubridate_1.7.4  rematch_1.0.1   
+    ## [41] assertthat_0.2.0 rmarkdown_1.11   httr_1.3.1       rstudioapi_0.8  
+    ## [45] R6_2.3.0         nlme_3.1-137     compiler_3.5.1
---
+++ functions.R
@@ -0,0 +1,88 @@
+run_jags <- function(data, file, em, N, thin){
+    jags.data <- list(y.13 = data$d13C,
+                      y.14 = data$D14C,
+                      age = data$age,
+                      n = nrow(data),
+                      em13.m = filter(em, Variable == "mean", Isotope == "d13C")$value,
+                      em14.m = filter(em, Variable == "mean", Isotope == "D14C")$value,
+                      em13.s = filter(em, Variable == "s.d.", Isotope == "d13C")$value,
+                      em14.s = filter(em, Variable == "s.d.", Isotope == "D14C")$value)
+    jm <- jags.model(file,
+                     data = jags.data,
+                     n.chains = 1, quiet=TRUE)
+    update(jm, 10000)
+    coda.samples(jm, c("p", "m.13", "m.14", "mu.13", "mu.14" , "scale.13", "scale.14"),
+                 n.iter = N, thin = thin) %>% 
+        as.matrix() %>% 
+        as_tibble()
+}
+
+
+plot_p <- function(out_young, out_old, data_young, data_old){
+    p_y <- get_p(out_young, data_young) %>% 
+        group_by(age, source) %>% 
+        summarise(mean = mean(value), 
+                  median = median(value), 
+                  q25 = quantile(value, .25),
+                  q75 = quantile(value, .75),
+                  q05 = quantile(value, .05),
+                  q95 = quantile(value, .95)) %>% 
+        filter(age < 4000) %>% 
+        mutate(period = "a) Late Holocene")
+    p_o <- get_p(out_old, data_old) %>% 
+        group_by(age, source) %>% 
+        summarise(mean = mean(value), 
+                  median = median(value), 
+                  q25 = quantile(value, .25),
+                  q75 = quantile(value, .75),
+                  q05 = quantile(value, .05),
+                  q95 = quantile(value, .95)) %>% 
+        mutate(period = "c) B/A-YD-HTM")
+    ggplot() + 
+        geom_ribbon(data = p_o, aes(x = age, ymax = q75, ymin = q25, fill = source), alpha = 0.4) +
+        geom_ribbon(data = p_y, aes(x = age, ymax = q75, ymin = q25, fill = source), alpha = 0.4) +
+        geom_line(data = p_o, aes(x = age, y = median, color = source)) +
+        geom_line(data = p_y, aes(x = age, y = median, color = source)) +
+        expand_limits(y = c(0,1)) + 
+        ylab("Source fraction") + 
+        scale_color_manual(values=c("olivedrab3", "darkorange1", "cadetblue3"), guide = guide_legend(title = NULL)) +
+        scale_fill_manual(values=c("olivedrab3", "darkorange1", "cadetblue3"), guide = guide_legend(title = NULL)) + 
+        xlab("Cal yr BP") + 
+        scale_y_continuous(expand = c(0, 0)) +
+        theme_bw() +
+        scale_x_continuous(breaks = seq(0, 13000, by = 500)) +
+        theme(strip.background = element_blank(), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+              legend.position="none",
+              panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+        geom_rug(data = bind_rows(data_young, data_old), aes(x = age)) 
+}
+
+plot_p2 <- function(out_young, out_old, data_young, data_old){
+  bind_rows(get_p(out_young, data_young), 
+            get_p(out_old, data_old)) %>% 
+    filter(age %in% filter(bind_rows(data_young, data_old), !is.na(D14C))$age) %>% 
+    mutate(age = round(age)) %>% 
+    mutate(label = fct_reorder(paste(age, "BP"), age)) %>% 
+    ggplot(aes(x=value, fill = source, color = source))+geom_density(alpha = .4) + 
+    facet_wrap(~ label) +
+    scale_fill_manual(values=c("olivedrab3", "darkorange1", "cadetblue3"), 
+                      guide = guide_legend(title = NULL)) +
+    scale_color_manual(values=c("olivedrab3", "darkorange1", "cadetblue3"), 
+                       guide = guide_legend(title = NULL)) + 
+    theme_bw() +
+    theme(legend.position=c(0.7, 0.05), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+          panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text.y=element_blank()) +
+    ylab("") + xlab("")
+}
+
+
+get_p <- function(coda_out, data){
+    coda_out %>% 
+        select(starts_with("p[")) %>% 
+        gather() %>% 
+        mutate(source = c("ICD", "Active Layer", "Marine OC")[as.numeric(str_sub(key, -2, -2))],
+               age = data$age[as.numeric(str_sub(key, 3, -4))])
+}
+
+
+
